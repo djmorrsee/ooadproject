90c0c5af60da7a6dce99d173656ecea58265d373
djmorrsee <djmorrsee@gmail.ocm>
2014-03-16 14:12:13 -0600

    xcode project

---
+++ app/xcode/CalendarApp/CalendarApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CalendarApp
+//
+//  Created by Daniel Morrissey on 3/16/14.
+//  Copyright (c) 2014 Daniel Morrissey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ app/xcode/CalendarApp/CalendarAppTests/CalendarAppTests.m
@@ -0,0 +1,34 @@
+//
+//  CalendarAppTests.m
+//  CalendarAppTests
+//
+//  Created by Daniel Morrissey on 3/16/14.
+//  Copyright (c) 2014 Daniel Morrissey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CalendarAppTests : XCTestCase
+
+@end
+
+@implementation CalendarAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
