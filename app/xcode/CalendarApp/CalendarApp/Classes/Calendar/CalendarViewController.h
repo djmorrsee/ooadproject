ac30cffbaef79912e7b0c22e9656a1d3f5053a38
Tom Spooner <spoonertb@gmail.com>
2014-04-26 14:50:58 -0600

    CalendarViewController Stuff

--- app/xcode/CalendarApp/CalendarApp/Classes/Calendar/CalendarViewController.h
+++ app/xcode/CalendarApp/CalendarApp/Classes/Calendar/CalendarViewController.h
@@ -16,4 +16,14 @@
 
 - (void)displaySelectedDate;
 
+@property (nonatomic, strong) NSDate *firstDate;
+
+@property (nonatomic, strong) NSDate *lastDate;
+
+@property (nonatomic, strong) NSDate *selectedDate;
+
+@property (nonatomic, strong) NSCalendar *calendar;
+
 @end
+
+
